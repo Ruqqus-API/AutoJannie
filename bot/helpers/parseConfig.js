191c91b78938181243ef36340461974a1753b435
Panjkrc <panjkrc.mihael@gmail.com>
2021-02-25 22:54:36 +0100

    Update parseConfig.js

--- bot/helpers/parseConfig.js
+++ bot/helpers/parseConfig.js
@@ -27,7 +27,7 @@ module.exports = {
 
 		const config = {
 			name: post.guild.name,
-			guildmasters: post.guild.guildmasters,
+			guildmasters: guildmasters,
 			rules: rules
 		}
 
@@ -42,7 +42,7 @@ module.exports = {
 					data: {
 						data: {
 							name: post.guild.name,
-							guildmasters: post.guild.guildmasters,
+							guildmasters: guildmasters,
 							rules: rules
 						}
 					}
@@ -69,7 +69,7 @@ module.exports = {
 				)
 			)
 		)
-			.then(res => {
+			.then(() => {
 				post.comment(`Config successfully saved to the database! Automoderator is now active`)
 			})
 			.catch(err => {
