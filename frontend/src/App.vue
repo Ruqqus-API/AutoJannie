fd6fe6294eab9c9e53239d9ba75afb28080f8b25
kek7198 <memezdank140@gmail.com>
2021-02-26 15:17:58 -0600

    divs

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,32 @@
 <template>
   <div id="app">
-    <Sidebar/>
-
-    <router-view></router-view>
+    <!-- Top navigation across the app -->
+    <Navbar/>
+    <!-- Sidebar and main content -->
+    <div class="flex h-screen pt-14">
+      <Sidebar/>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-import Sidebar from "@/components/Navigation/Sidebar.vue"
 
-export default {
-  name: 'App',
-  components: {
-    Sidebar
-  },
-  computed: {
-    count() {
-      return this.$store.state.count
+  import Navbar from "@/components/Navigation/Navbar.vue"
+  import Sidebar from "@/components/Navigation/Sidebar.vue"
+
+  export default {
+    name: 'App',
+    components: {
+      Navbar,
+      Sidebar
+    },
+    computed: {
+      count() {
+        return this.$store.state.count
+      }
     }
   }
-}
 </script>
 
 <style>
--- frontend/src/components/Navigation/Sidebar.vue
+++ frontend/src/components/Navigation/Sidebar.vue
@@ -1,4 +1,4 @@
 <template>
-	<div>
+	<div class="hidden md:flex items-center flex-col flex-shrink-0 w-64 border-r bg-purple-500 dark:bg-gray-750 dark:border-white-13">
 	</div>
 </template>
