a5f46a4b0bbf0413aa6a4ebb6971c57c8db6a6a4
ax3cutor <69725765+ax3cutor@users.noreply.github.com>
2021-02-26 23:25:20 +0000

    vue-tailwind

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import Vuex from '../node_modules/vuex'
 import store from "./store/index.js"
 import vuescroll from "../node_modules/vuescroll"
+import VueTailwind from "vue-tailwind"
 
 // import FontAwesome
 import "./assets/fontawesome/css/all.css";
@@ -13,6 +14,10 @@ import "vuescroll/dist/vuescroll.css";
 // import custom style
 import "../node_modules/tailwindcss/tailwind.css"
 import "./assets/css/style.css";
+
+import settings from './settings';
+
+Vue.use(VueTailwind, settings);
 
 // Vuescroll
 Vue.use(vuescroll, {
