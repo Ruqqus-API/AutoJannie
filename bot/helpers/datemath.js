5c8b48762451b44b3ecaad75c4cf8986a0108a9f
Panjkrc <panjkrc.mihael@gmail.com>
2021-03-05 22:02:03 +0100

    Update datemath.js

--- bot/helpers/datemath.js
+++ bot/helpers/datemath.js
@@ -1,23 +1,21 @@
 function calculatePassedTime(amount, unit) {
-    switch (unit) {
-        case "second":
-            return amount
-        case "minute":
-            return amount * 60;
-        case "hour":
-            return amount * 60 * 60;
-        case "day":
-            return amount * 60 * 60 * 24;
-        case "week":
-            return amount * 60 * 60 * 24 * 7;
-        case "month":
-            return amount * 60 * 60 * 24 * 7 * 4;
-        case "year":
-            return amount * 60 * 60 * 24 * 7 * 4 * 12;
-        default: return new Date();
-    }
+	switch (unit) {
+		case "second":
+			return amount
+		case "minute":
+			return amount * 60;
+		case "hour":
+			return amount * 60 * 60;
+		case "day":
+			return amount * 60 * 60 * 24;
+		case "week":
+			return amount * 60 * 60 * 24 * 7;
+		case "month":
+			return amount * 60 * 60 * 24 * 7 * 4;
+		case "year":
+			return amount * 60 * 60 * 24 * 7 * 4 * 12;
+		default: return new Date();
+	}
 }
 
-export default {
-    calculatePassedTime
-}
+module.exports = { calculatePassedTime }
