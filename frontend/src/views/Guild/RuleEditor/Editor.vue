7a5ee146550eec23ac360246d6695add339e04d5
ax3cutor <69725765+ax3cutor@users.noreply.github.com>
2021-03-13 22:35:57 +0000

    binding

--- frontend/src/views/Guild/RuleEditor/Editor.vue
+++ frontend/src/views/Guild/RuleEditor/Editor.vue
@@ -32,9 +32,9 @@
 								<div class="space-y-2 p-3 bg-white border border-t-0 rounded-b">
 									<div class="flex items-center space-x-2">
 										<div class="flex items-center justify-center px-2 w-9 h-9 bg-gray-100 shadow-inner font-bold rounded text-gray-500 hover:text-gray-800">
-											<i class="fas fa-link fa-fw fa-sm"></i>
+											<i class="fas fa-fw fa-sm" :class="$store.state.triggers.picked.icon"></i>
 										</div>
-										<div class="capitalize font-medium">Link post</div>
+										<div class="capitalize font-medium">{{ $store.state.triggers.picked.name }}</div>
 									</div>
 								</div>
 							</div>
