d22acc97f102e8e65da5d50a26eda9a3f90a4834
ax3cutor <69725765+ax3cutor@users.noreply.github.com>
2021-03-13 00:01:16 +0000

    Update settings.js

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -4,6 +4,7 @@ import {
 	TTextarea,
 	TSelect,
 	TRadio,
+	TRadioGroup,
 	TCheckbox,
 	TButton,
 	TInputGroup,
@@ -61,6 +62,34 @@ const settings = {
 				error: 'text-red-500 border-red-300 focus:border-red-500 focus:ring-red-500',
 				success: 'text-green-500 border-gray-300 focus:border-green-500 focus:ring-green-500',
 				warning: 'text-yellow-500 border-yellow-300 focus:border-yellow-500 focus:ring-yellow-500'
+			}
+		}
+	},
+	't-radio-group': {
+		component: TRadioGroup,
+		props: {
+			classes: {
+				groupWrapper: 'flex flex-col',
+				label: 'ml-2 text-gray-700 uppercase text-sm',
+				input: 'text-blue-500 transition duration-100 ease-in-out border-gray-300 shadow-sm focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 focus:ring-offset-0 disabled:opacity-50 disabled:cursor-not-allowed'
+
+			},
+			variants: {
+				danger: {
+					groupWrapper: 'flex flex-col',
+					label: 'ml-2 text-red-500 uppercase text-sm',
+					input: 'text-red-500 transition duration-100 ease-in-out border-red-500 shadow-sm focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 focus:ring-offset-0 disabled:opacity-50 disabled:cursor-not-allowed'
+				},
+    				buttons: {
+					groupWrapper: 'flex',
+					label: '',
+					labelChecked: '',
+    					wrapper: 'mx-1 bg-white border border-gray-300 flex items-center px-4 py-2 rounded shadow-sm cursor-pointer focus:shadow-outline text-sm text-gray-700 hover:text-gray-500 leading-5 uppercase',
+					wrapperChecked: 'mx-1 bg-gray-100 border border-gray-300 flex items-center px-4 py-2 rounded shadow-inner cursor-pointer focus:shadow-outline text-sm text-gray-700 hover:text-gray-500 leading-5 uppercase',
+					inputWrapper: '',
+					inputWrapperChecked: '',
+					input: 'absolute invisible'
+				}
 			}
 		}
 	},
@@ -170,7 +199,7 @@ const settings = {
 			classes: {
 				wrapper: '',
 				buttonWrapper: '',
-				selectButton: 'bg-white border-gray-300',
+				selectButton: 'bg-light border-gray-300',
 				selectButtonLabel: '',
 				selectButtonPlaceholder: 'text-gray-400',
 				selectButtonIcon: 'text-gray-600',
