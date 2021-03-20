8f6750084430e8b44375965c2587b3e5637a29af
Panjkrc <panjkrc.mihael@gmail.com>
2021-03-20 12:44:37 +0100

    Update invited.js

--- bot/helpers/commands/invited.js
+++ bot/helpers/commands/invited.js
@@ -5,7 +5,7 @@ module.exports = {
 	name: 'invited',
 	description: 'Checks for GM invite',
 	usage: `${command_prefix} invited`,
-	aliases: ['i', 'check'],
+	aliases: ['i', 'check', 'update'],
 	async execute({ client, faunaClient, comment, redisClient }) {
 		let post = await client.posts.fetch(comment.post.id)
 
